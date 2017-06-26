f79e54747d423da920430f756390d803f332e918
Kirk Trachy <ktrachy@users.noreply.github.com>
2017-06-25 20:46:25 -0400

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,12 @@
+< script >
+    var word = ["l", "i", "n", "c", "o", "l", "n"];
+var responsearray = [];
 
+function putInUnderscores() {
+
+    for (var i = 0; i < word.length; i++)
+    // Logs the animal at index position i to the console. This code is executed each we go through the loop.
+        responsearry === "_";
+    console.log(word[i]);
+} < /script>
+putInUnderscores
